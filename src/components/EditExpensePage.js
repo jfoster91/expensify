78db4c7420dd2783240c37b956b84c1459e8e131
jfoster91 <jonathan.j.foster91@gmail.com>
2020-05-08 08:03:18 +0100

    Test basic params for edit page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-    <div>
-        Edit Expense
-    </div>
-)
+const EditExpensePage = (props) => {
+    console.log(props)
+    return (   
+        <div>
+            Edit Expense with id of { props.match.params.id }
+        </div>
+    )
+ }
 
 export default EditExpensePage
