1e1004c8583fe5e34deb54ffb439afa3dbc2ab5e
jfoster91 <jonathan.j.foster91@gmail.com>
2020-05-08 07:18:15 +0100

    Basic Routes and 404

--- src/app.js
+++ src/app.js
@@ -1,8 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const ExpenseDashboardPage = () => (
+    <div>
+        Expense DashBoard
+    </div>
+)
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+const AddExpensePage = () => (
+    <div>
+        Add Expense
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+        Edit Expense
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+        Help Page
+    </div>
+)
+
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
