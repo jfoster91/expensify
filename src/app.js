7782f83f08f9da32854d582e324cc33fd84694b6
jfoster91 <jonathan.j.foster91@gmail.com>
2020-05-08 07:26:51 +0100

    Set up basic navigation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -35,15 +35,29 @@ const NotFoundPage = () => (
     </div>
 )
 
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>DashBoard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+
+    </header>
+)
+
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
